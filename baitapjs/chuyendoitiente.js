f5939c6cbfc8e71a4233aab28d79bf34dc83e846
Your Name <you@example.com>
2022-06-08 10:23:12 +0700

    done

--- baitapjs/chuyendoitiente.js
+++ baitapjs/chuyendoitiente.js
@@ -1,18 +1,61 @@
 function sum() {
     let a = +document.getElementById("soluong").value;
-    let b = 0;
+    let b;
     let c = document.getElementById("from").value;
     let d = document.getElementById("to").value;
-    if(c == "USD") {
-        if (d == "VND")
-            b = a * 23000;
-        else b = a
+    let e;
+    switch (c) {
+        case "USD":
+            e = a*23000
+            break
+        case "VND":
+            e = a*1
+            break
+        case "AUD":
+            e = a*17000
+            break
+        case "EUR":
+            e = a*25000
+            break
+        case "JPY":
+            e = a*178
+            break
+        case "GBP":
+            e = a*29800
+            break
+        case "CHF":
+            e = a*24000
+            break
+        case "CAD":
+            e = a*19000
+            break
     }
-    else {
-        if (d == "VND")
-            b = a
-        else
-            b = a / 23000
+    switch (d){
+        case "VND":
+            b = e
+            break
+        case "USD":
+            b = e/23000
+            break
+        case "AUD":
+            b = e/17000
+            break
+        case "EUR":
+            b = e/25000
+            break
+        case "JPY":
+            b = e/178
+            break
+        case "GBP":
+            b = e/29800
+            break
+        case "CHF":
+            b = e/24000
+            break
+        case "CAD":
+            b = e/19000
+            break
     }
+
     document.getElementById("kq").innerHTML = b;
 }
