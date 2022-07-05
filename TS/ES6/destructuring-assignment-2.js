03bbc91b98ec4cec102d3ab9a99a8b737d9c034c
Your Name <you@example.com>
2022-07-05 08:47:11 +0700

    oop1

---
+++ TS/ES6/destructuring-assignment-2.js
@@ -0,0 +1,21 @@
+function detectCollision(objects, point) {
+    // for (let i = 0; i < objects.length; i++) {
+    //     let object = objects[i]
+    //     if (point.x >= object.x && point.x <= object.x + object.width &&
+    //         point.y >= object.y && point.y <= object.y + object.height)
+    //         return object
+    //
+    // }
+    return objects.find((object)=>{
+        if (point.x >= object.x && point.x <= object.x + object.width &&
+            point.y >= object.y && point.y <= object.y + object.height)
+            return object
+    })
+}
+const myObjects = [
+    {x:  10, y: 20, width: 30, height: 30},
+    {x: -40, y: 20, width: 30, height: 30},
+    {x:   0, y:  0, width: 10, height:  5}
+]
+
+console.log(detectCollision(myObjects, {x: 4, y: 2}))
