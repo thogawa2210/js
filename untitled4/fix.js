6560338eef8442b7a09d5a4eb9e46e10d8fb0ac7
Your Name <you@example.com>
2022-07-06 09:06:10 +0700

    ts4

---
+++ untitled4/fix.js
@@ -0,0 +1,35 @@
+function main() {
+    var message;
+    var total = 100;
+    var isProduction = true;
+    var prices = [120, 88, 60];
+    var languages = ['vi', 'en-us'];
+    var now = new Date();
+    var unknown;
+    var Direction;
+    (function (Direction) {
+        Direction[Direction["UP"] = 0] = "UP";
+        Direction[Direction["DOWN"] = 1] = "DOWN";
+        Direction[Direction["LEFT"] = 2] = "LEFT";
+        Direction[Direction["RIGHT"] = 3] = "RIGHT";
+    })(Direction || (Direction = {}));
+    ;
+    function log(msg) {
+        console.log(msg);
+    }
+    isProduction = false;
+    unknown = Direction.UP;
+    unknown = 'changed';
+    var post = { id: 'a', title: 'b' };
+    message = '50';
+    function getPost(postId) {
+        // do something to retrieve post
+        return {
+            id: postId,
+            title: 'Post Title',
+            body: 'Post Body',
+            extra: 'data'
+        };
+    }
+}
+main();
