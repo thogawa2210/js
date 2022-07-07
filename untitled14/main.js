d51a42f49f0183ef4fd0aba044ca852b9b6e9f1d
Your Name <you@example.com>
2022-07-07 14:41:05 +0700

    ts14

---
+++ untitled14/main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var readLineSync = require("readLine-sync");
+var input = readLineSync.question('Please enter your number');
+console.log(input);
---
+++ untitled14/main.ts
@@ -0,0 +1,5 @@
+import * as readLineSync from 'readLine-sync';
+
+let input = readLineSync.question('Please enter your number: ');
+console.log(input);
+
