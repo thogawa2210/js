3ebe37b5b55ea6e2d6da639b76200e51fa60c364
Your Name <you@example.com>
2022-07-10 21:57:09 +0700

    ts23

---
+++ untitled23/Calculator.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+exports.Calculator = void 0;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.calculate = function (firstOperand, secondOperand, operator) {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    console.log("Can not divide by 0");
+            default:
+                console.log("Unsupported operation");
+        }
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
---
+++ untitled23/Calculator.ts
@@ -0,0 +1,19 @@
+export class Calculator {
+    public static calculate(firstOperand: number, secondOperand: number, operator: string): number {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    console.log("Can not divide by 0");
+            default:
+                console.log("Unsupported operation");
+        }
+    }
+}
