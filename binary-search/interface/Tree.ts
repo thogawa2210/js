91378daf4b9dce2d034834bf0a73479354d30051
Your Name <you@example.com>
2022-07-12 16:34:48 +0700

    binary-search

---
+++ binary-search/interface/Tree.ts
@@ -0,0 +1,7 @@
+import {TreeNode} from "../TreeNode";
+
+export interface Tree<E> {
+    insert(data: E): TreeNode<E>;
+    inorder(node: TreeNode<E>): void;
+    getSize(): number;
+}
---
+++ binary-search/main.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var BinaryTree_1 = require("./src/BinaryTree");
+var tree = new BinaryTree_1.BinaryTree();
+tree.insert(6);
+tree.insert(5);
+tree.insert(7);
+tree.insert(3);
+tree.insert(4);
+console.log("The numbers of nodes ".concat(tree.totalNode));
+tree.inorder(tree.root);
