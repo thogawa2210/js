727c3a1a28cbd53fa2cd7d552405dee3aa136d90
Your Name <you@example.com>
2022-07-13 08:59:32 +0700

    TDD

--- untitled26/src/app.ts
+++ untitled26/src/app.ts
@@ -9,17 +9,53 @@
 //     }
 // }
 
-export class BasicCalculator{
-    add(a: number, b: number): number {
-        return a + b;
-    }
-    subtract(a: number, b: number): number {
-        return a - b;
-    }
-    multiple(a: number, b: number): number {
-        return a * b;
-    }
-    divide(a: number, b: number): number {
-        return a/b;
+// export class BasicCalculator{
+//     add(a: number, b: number): number {
+//         return a + b;
+//     }
+//     subtract(a: number, b: number): number {
+//         return a - b;
+//     }
+//     multiple(a: number, b: number): number {
+//         return a * b;
+//     }
+//     divide(a: number, b: number): number {
+//         return a/b;
+//     }
+// }
+
+// export class TriangleClassifier{
+//     classifier(a:number, b:number,c:number){
+//         if( a>0 && b>0 && c>0){
+//             if( a+b>c && b+c>a && c+a>b){
+//                 if(a === b && a === c){
+//                     return 'tam giác đều';
+//                 }else if(a === b || a === c || b === c){
+//                     return 'tam giác cân';
+//                 }else{
+//                     return 'tam giác thường';
+//                 }
+//             }else{
+//                 return 'không phải tam giác';
+//             }
+//         }else{
+//             return 'không phải tam giác';
+//         }
+//     }
+// }
+
+export class FizzBuzzTranslate {
+    translate(number: number) {
+        if (number % 3 === 0 && number % 5 === 0) {
+            return 'FizzBuzz';
+        } else {
+            if (number % 3 === 0) {
+                return 'Fizz';
+            } else if (number % 5 === 0) {
+                return 'Buzz';
+            }else{
+                return `${number}`;
+            }
+        }
     }
 }
--- untitled26/src/test/app.spec.ts
+++ untitled26/src/test/app.spec.ts
@@ -27,33 +27,73 @@
 //     });
 // });
 
-import {BasicCalculator} from "../app";
+// import {BasicCalculator} from "../app";
+//
+// describe('BasicCalulator Testing', () => {
+//     let calc: BasicCalculator = new BasicCalculator();
+//
+//
+//     test('should return an instance of a basic calculator', () => {
+//         expect(calc).toEqual({});
+//     });
+//
+//
+//     test('should return the addition of a + b', () => {
+//         expect(calc.add(1,3)).toEqual(4);
+//     });
+//
+//
+//     test('should return the subtraction of a - b', () => {
+//         expect(calc.subtract(1,3)).toEqual(-2);
+//     });
+//
+//
+//     test('should return the multple of a * b', () => {
+//         expect(calc.multiple(1,3)).toEqual(3);
+//     });
+//
+//
+//     test('should return the divide of a / b', () => {
+//         expect(calc.divide(1,3)).toEqual(1/3);
+//     });
+// });
 
-describe('BasicCalulator Testing', () => {
-    let calc: BasicCalculator = new BasicCalculator();
+// import {TriangleClassifier} from "../app";
+//
+// describe('BasicCalulator Testing', () => {
+//     let triangleClassifier = new TriangleClassifier();
+//
+//     test('kết quả là tam giác đều', () => {
+//         expect(triangleClassifier.classifier(2, 2, 2)).toBe('tam giác đều');
+//     });
+//     test('kết quả là tam giác cân', () => {
+//         expect(triangleClassifier.classifier(2, 2, 3)).toBe('tam giác cân');
+//     });
+//     test('kết quả là tam giác thường', () => {
+//         expect(triangleClassifier.classifier(3, 4, 5)).toBe('tam giác thường');
+//     });
+//     test('kết quả là không phải tam giác', () => {
+//         expect(triangleClassifier.classifier(8, 2, 3)).toBe('không phải tam giác');
+//     });
+//     test('kết quả là không phải tam giác', () => {
+//         expect(triangleClassifier.classifier(0, 1, 1)).toBe('không phải tam giác');
+//     });
+//     test('kết quả là không phải tam giác', () => {
+//         expect(triangleClassifier.classifier(-1, 2, 1)).toBe('không phải tam giác');
+//     });
+// })
 
+import {FizzBuzzTranslate} from "../app";
 
-    test('should return an instance of a basic calculator', () => {
-        expect(calc).toEqual({});
-    });
+describe('FizzBuzz Testing', () => {
+    let fizzbuzz = new FizzBuzzTranslate();
 
-
-    test('should return the addition of a + b', () => {
-        expect(calc.add(1,3)).toEqual(4);
-    });
-
-
-    test('should return the subtraction of a - b', () => {
-        expect(calc.subtract(1,3)).toEqual(-2);
-    });
-
-
-    test('should return the multple of a * b', () => {
-        expect(calc.multiple(1,3)).toEqual(3);
-    });
-
-
-    test('should return the divide of a / b', () => {
-        expect(calc.divide(1,3)).toEqual(1/3);
-    });
-});
+    test('the result should be fizz',() => {
+        expect(fizzbuzz.translate(9)).toBe('Fizz')});
+    test('the result should be buzz',() => {
+        expect(fizzbuzz.translate(10)).toBe('Buzz')});
+    test('the result should be fizzbuzz',() => {
+        expect(fizzbuzz.translate(30)).toBe('FizzBuzz')});
+    test('the result should be number',() => {
+        expect(fizzbuzz.translate(7)).toBe('7')});
+})
