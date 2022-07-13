2687d75769f52b91fa77a19d839f0272979788e6
Your Name <you@example.com>
2022-07-13 15:44:50 +0700

    ts41

---
+++ untitled41/main.ts
@@ -0,0 +1,21 @@
+let viettel = [];
+let vinaphone = [];
+let mobiphone = [];
+
+function classify(array: string[]){
+    for(let i=0; i<array.length; i++){
+        if(array[i].substring(0,3) === '098'){
+            viettel.push(array[i]);
+        }else if(array[i].substring(0,3) === '091'){
+            vinaphone.push(array[i]);
+        }else{
+            mobiphone.push(array[i]);
+        }
+    }
+}
+
+let phoneNumberArray = ['0987654321', '0912345678', '0909099999', '0988666666', '0987111111', '0915555555', '0906274116'];
+classify(phoneNumberArray);
+console.log(viettel);
+console.log(vinaphone);
+console.log(mobiphone);
